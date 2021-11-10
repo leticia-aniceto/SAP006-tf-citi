8d391fb682142d7e39bdc83bec5e7d68bfa1d9dd
Letícia Braga Aniceto <leticia.aniceto@usp.br>
2021-11-10 17:01:17 -0300

    third try

--- src/Root.js
+++ src/Root.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/home.js';
 import About from './pages/about.js';
 import Investing from './pages/investing.js';
-import Tickers from './pages/tickers.js';
 import NotFound from './pages/notFound.js';
 import SearchPage from './pages/search.js';
 
@@ -14,7 +13,6 @@ const Root = () => {
         <Route path='/' exact component={Home} />
         <Route path='/about' exact component={About} />
         <Route path='/investing' exact component={Investing} />
-        <Route path='/alltickers' exact component={Tickers} />
         <Route path='/search' exact component={SearchPage} />
         <Route component={NotFound} />
       </Switch>
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,61 +1,107 @@
 import React from "react";
-import { useState } from "react";
-// import Button from '../components/button'
+import { useState, useEffect } from "react";
 import Navbar from "../components/navbar";
 import Header from "../components/header";
-import '../style.css'
-import Chart from "./chart";
-import {searchName} from "../services/alphavantage.js"
+import "../style.css";
+import "./search.css";
+import { searchName, searchStock } from "../services/alphavantage.js";
 
-const SearchPage = () =>{
-    const [cards, setCards] = useState([])
-    const [searchValue, setSearchValue] = useState() 
-    const handleChange = e => {
-        setSearchValue(e.target.value)
+const SearchPage = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [cards, setCards] = useState([]);
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  useEffect(() => {
+    if (searchValue.length > 2) {
+      searchName(searchValue).then((result) => {
+        if (result.data["bestMatches"]) {
+          setSearchResult(result.data["bestMatches"].slice(0, 3));
+        }
+      });
+    } else {
+      if (searchResult.length > 0) setSearchResult([])
     }
-    const searchSymbol = (e) => {
-        e.preventDefault()
-        searchName(searchValue).then((result) => {
-            setCards(result.data['bestMatches'])
-        })
-    }
-return (
+  }, [searchValue]);
+
+  const createCard = (e) => {
+    searchStock(e.target.getAttribute("symbol")).then((result) => {
+      if (result.status === 200) {
+        let stock = result.data["Global Quote"];
+        setCards((cards) => [...cards, stock]);
+      }
+    });
+    setSearchValue("");
+    setSearchResult([])
+  };
+
+  return (
     <>
       <Header />
       <Navbar />
       <div className="">
         <div className="container justify-content-center">
           <div className="title">
-            <h2>Empresas na bolsa de Nova York</h2>
-
+            <h2>Busca de empresas globais</h2>
+            <h5 className="title">Acompanhe em tempo real todas as ações e seus valores</h5>
           </div>
-          {/* <form className="row gy-2 gx-3 justify-content-center"> */}
           <div className="row">
-            <div className="col-5">
-              <form>
-                <label>Digite o nome ou tickers da empresa</label>
-                <input type="text" onChange={handleChange}/>
-                {/* o botao fica desabilitado se os intervalos nao forem selecionados atraves da propriedade disabled */}
-                <button onClick={searchSymbol} className="btn btn-dark btn-block mt-2">Consultar</button> 
+            <div className="col-12">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Digite o nome da empresa"
+                onChange={handleChange}
+                value={searchValue}
+              />
+              <ul className="list-group">
+                {searchResult.map((item) => {
+                  return (
+                    <li
+                      key={item["1. symbol"]}
+                      onClick={createCard}
+                      symbol={item["1. symbol"]}
+                      className="list-group-item cursor-pointer"
+                    >
+                      {item["1. symbol"] + " - " + item["2. name"]}
+                    </li>
+                  );
+                })}
+              </ul>
+              <hr/>
+              <section>
+                {cards.map((item) => {
+                  return (
+                    <div
+                      className="card-ticker"
+                      key={item["01. symbol"]}
+                    >
+                      <div className="card-name">
+                        <h4 className="card-title">{item["01. symbol"]}</h4>
+                      </div>
 
-              </form>
-              <ul className="cards">
-              {cards.map((item) => {
-                  console.log(item)
-                return (
-                  <>
-                    {item['2. name']}
-                  </>
-                );
-              })}
-            </ul>
+                      <div className="card-data">
+                        <p className="open">
+                          <strong>Abertura</strong> <br />
+                          $ {item["02. open"]}
+                        </p>
+                        <p className="close">
+                          <strong>Último fechamento</strong> <br />
+                          $ {item["05. price"]}
+                        </p>
+                      </div>
+                    </div>
+                  );
+                })}
+              </section>
             </div>
-
           </div>
         </div>
       </div>
     </>
-
   );
 };
 
